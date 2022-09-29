ff4fda1f84bb8e3019927a3cde3da0352e7374af
ayakub <ayakub1086@gmail.com>
2022-09-29 21:04:04 +0600

    added fonawesome

--- src/Componants/Nav/Nav.js
+++ src/Componants/Nav/Nav.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Nav.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWalking } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = () => {
     return (
         <div className='nav-container'>
-            <h2> Everydays Activities</h2>
+            <h2><FontAwesomeIcon icon={faWalking}></FontAwesomeIcon> Everydays Activities</h2>
             <h4>Select's Your Activities</h4>
+
         </div>
     );
 };
